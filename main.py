9a75d13de6c3672c593b2aa3fc0571ed3bad4ee7
SamuelJEAlves <samuelalvesevangelista@gmail.com>
2023-09-24 19:57:23 -0300

    Ajustes de nomenclatura.

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from cryptography.fernet import Fernet
 
-secretKey = Fernet.generate_key()
+secret_key = Fernet.generate_key()
 
-fernet = Fernet(secretKey)
+fernet = Fernet(secret_key)
 
-def EncryptPassword(password):
+def encrypt_password(password):
     return fernet.encrypt(password.encode()).decode()
 
-def DecryptPassword(encryptedPassword):
-    return fernet.decrypt(encryptedPassword.encode()).decode()
+def decrypt_password(encrypted_password):
+    return fernet.decrypt(encrypted_password.encode()).decode()
 
-mainPassword = input('Enter password:')
-encryptedPassword = EncryptPassword(mainPassword)
-decryptedPassword = DecryptPassword(encryptedPassword)
+main_password = input('Enter password:')
+encrypted_password = encrypt_password(main_password)
+decrypted_password = decrypt_password(encrypted_password)
 
-print(f'Senha Desprotegida: {mainPassword}')
-print(f'Senha Criptografada: {encryptedPassword}')
-print(f'Senha Descriptografada: {decryptedPassword}')
+print(f'Senha Desprotegida: {main_password}')
+print(f'Senha Criptografada: {encrypted_password}')
+print(f'Senha Descriptografada: {decrypted_password}')
